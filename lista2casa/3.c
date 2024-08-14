7ebd9b0b90e1388aa269e6f36cb1ccd0fbc944b0
giovanna.mrts <gigiozetto@gmail.com--unset>
2024-08-14 17:42:11 -0300

    update

---
+++ lista2casa/3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(){
+    float n, nvn;
+    printf("digite a nota:");
+    scanf("%f", &n);
+    if(n%1!=0){
+        if(n>0.5){
+        int n;
+        nvn=n+1;
+        }
+    }
+    else{
+        int n;
+        nvn=n;
+    }
+    printf("%f", nvn);
+}
